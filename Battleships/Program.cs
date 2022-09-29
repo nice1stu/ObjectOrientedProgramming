5b3312891ee4b6af81ade22342173d1bdf391396
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-09-29 22:51:08 +0200

    Test grid printing array

--- Battleships/Program.cs
+++ Battleships/Program.cs
@@ -1,2 +1,85 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*2 Players
+10 x 10 grid per player
+Simplified Ship Sizes: Only 5x 1 sized ships
+Players take alternating turns
+Coordinates given in the form g7, a2, d2 etc...
+consider using the numeric value of chars! :)
+can be simplified at first with two inputs:
+first: int x
+then: int y
+Feedback "Hit" or "Miss"
+Win Condition: All enemy ships have been sunk
+Bonus Rules (A*):
+Ships: sizes 5, 4, 3, 2 ships with 2
+Ships may be placed vertically or horizontally
+Ships may not overlap and may not touch each other (i.e. there needs to be at least 1 empty grid cell around the ship in every direction)
+Feedback "Ship sunk!"
+Bonus Bonus (No score):
+A.I.? :)
+Radar Power Up
+Can be used to detect ships in a 3x3 radius around the location placed (but not hit them)
+Can you think of other power-ups? :)
+Scanner Power Up
+Can be used to scan a 3x3 radius around the location placed
+Only returns "Ship detected" or "No ship detected"*/
+
+Console.WriteLine("Lets try to make Battleship");
+
+//declare arrays & variables
+char[,] player1FleetMap = new char[9, 9];
+char[,] player1AttackMap = new char[9, 9];
+char[,] player2FleetMap = new char[9, 9];
+char[,] player2AttackMap = new char[9, 9];
+string[,] paintToolArray = new String[5,5];
+
+/*for (int x = 0; x < player1AttackMap.Length; x++)
+{
+    for (int y = 0; y < player1AttackMap.Length; y++)
+    {
+        player1AttackMap[x, y] = 'O';
+    }
+}*/
+
+//initialize all values of array
+for (int y = 0; y < 5; y++)
+{
+    for (int x = 0; x < 5; x++)
+    {
+        paintToolArray[x, y] = " ";
+    }
+}
+Maps();
+//draw maps
+void Maps()
+{
+    Console.WriteLine("Y");
+    for (int x = 0; x < 10; x++)
+    {
+        for (int y = 0; y < 10; y++)
+        {
+            Console.Write("~");
+        }
+        Console.WriteLine();
+    }
+}
+PrintXYGrid();
+//print paintToolArray
+void PrintXYGrid()
+{
+    Console.WriteLine("Y");
+    for (int y = 0; y < 5; y++)
+    {
+        Console.Write($"{4-y}");
+        for (int x = 0; x < 5; x++)
+        {
+            Console.Write($"{paintToolArray[x,(4-y)]}");
+        }
+        Console.WriteLine();
+    }
+    Console.Write("\\");
+    for (int x = 0; x < 5; x++)
+    {
+        Console.Write(x);
+    }
+    Console.Write("X");
+}

9e906ba3d17d813600c7d3830318a05c9e2b4ec9
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-09-30 21:37:27 +0200

    Add axial co-ordinates

--- Battleships/Program.cs
+++ Battleships/Program.cs
@@ -52,34 +52,20 @@ Maps();
 //draw maps
 void Maps()
 {
-    Console.WriteLine("Y");
+    Console.WriteLine("(Y)");
     for (int x = 0; x < 10; x++)
     {
+        Console.Write(9-x);
         for (int y = 0; y < 10; y++)
         {
             Console.Write("~");
         }
         Console.WriteLine();
     }
-}
-PrintXYGrid();
-//print paintToolArray
-void PrintXYGrid()
-{
-    Console.WriteLine("Y");
-    for (int y = 0; y < 5; y++)
-    {
-        Console.Write($"{4-y}");
-        for (int x = 0; x < 5; x++)
-        {
-            Console.Write($"{paintToolArray[x,(4-y)]}");
-        }
-        Console.WriteLine();
-    }
     Console.Write("\\");
-    for (int x = 0; x < 5; x++)
+    for (int x = 0; x < 10; x++)
     {
         Console.Write(x);
     }
-    Console.Write("X");
+    Console.Write("(X)");
 }

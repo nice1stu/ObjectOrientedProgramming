f234b26ac896c50623c156ce630dd60713f6aba3
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-10-04 22:36:12 +0200

    for loop for unit names working

--- RPG02/Program.cs
+++ RPG02/Program.cs
@@ -1,16 +1,36 @@
 ﻿// Lets try the RPG again
 
-/* For testing
+/* Testing
 Unit woofs = new Unit("Woofs");
 Unit bello = new Unit("Bello");
 Console.WriteLine(woofs.name);
 Console.WriteLine(bello.name);*/
 
+string[] units = new string[3];
+for (int i = 0; i < units.Length; i++)
+{
+    Console.WriteLine("Please name your unit");
+    string userInput = Console.ReadLine();
+    units[i] = userInput;
+}
+/* Testing
+ Console.WriteLine(units[0]);
+Console.WriteLine(units[1]);
+Console.WriteLine(units[2]);*/
+
 public class Unit
 {
     public string name;
+    public int iD;
+    public static int nextId;
     public Unit(string name)
     {
         this.name = name;
+        this.iD = nextId;
+        nextId++;
+    }
+    public void ReportStatus()
+    {
+        Console.WriteLine($"Unit #{iD}: {name}");
     }
 }

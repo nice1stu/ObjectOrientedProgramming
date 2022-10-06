82b58a6f5a2d88e7c2bab451e94a095a2a54ebef
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-10-06 21:55:28 +0200

    tried and removed Health Property

--- RPG02/Program.cs
+++ RPG02/Program.cs
@@ -1,15 +1,19 @@
 ﻿// Lets try the RPG
 
-/*string[] units = new String[3];
-for (int i = 0; i < units.Length; i++)
+Unit unit = new Unit("Skeleton", 100);
+unit = new Unit("Zombie", 200);
+unit = new Unit("Necromancer",300);
+unit = new Unit("Leet", 1337);
+
+
+
+/*for(int i = 0; i < 3; i++)
 {
-    Console.WriteLine("Please name your unit");
-    string userInput = Console.ReadLine();
-    units[i] = new string(userInput);
+    new Unit("Leet",1337);
+    //Console.WriteLine("What do you want Leet's Health to be?");
+    //int newHealth = Convert.ToInt32(Console.ReadLine());
+    //unit.SetHealth(newHealth);
 }*/
-//Unit unit = new Unit("Skeleton", 100);
-//unit = new Unit("Zombie", 200);
-//unit = new Unit("Necromancer",300);
 
 /*Testing Finalizer
 for(int i = 0; i < 2; i++)
@@ -17,15 +21,6 @@ for(int i = 0; i < 2; i++)
     unit = new Unit("LivingHand",400);
     GC.Collect();
 }*/
-
-for(int i = 0; i < 3; i++)
-{
-    new Unit("Leet",1337);
-    //Console.WriteLine("What do you want Leet's Health to be?");
-    //int newHealth = Convert.ToInt32(Console.ReadLine());
-    //unit.SetHealth(newHealth);
-}
-
 public class Unit
 {
     private string name;
@@ -33,15 +28,13 @@ public class Unit
     public static int nextId;
     private int maxHealth;
     private int health;
+    
 
     public Unit(string name, int maxHealth)
     {
         this.name = name;
         this.maxHealth = maxHealth;
         SetHealth();
-        //Console.WriteLine("What do you want Leet's Health to be?");
-        //health = Convert.ToInt32(Console.ReadLine());
-        //health = Math.Clamp(health, 0, maxHealth);
         iD = nextId;
         nextId++;
         ReportStatus();// Make sure, that this is the last line of the constructor:
@@ -49,7 +42,7 @@ public class Unit
     public void SetHealth()
     {
         int newHealth;
-        Console.WriteLine("What do you want Leet's Health to be?");
+        Console.WriteLine($"What do you want {name}'s Health to be?");
         newHealth = Convert.ToInt32(Console.ReadLine());
         health = newHealth;
         health = Math.Clamp(health, 0, maxHealth);

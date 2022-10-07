3211878e6377358099097a62dcb92a6c0527517a
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-10-07 16:23:25 +0200

    regress back to last working state

--- RPG02/Program.cs
+++ RPG02/Program.cs
@@ -1,26 +1,27 @@
 ﻿// Lets try the RPG
-
+/*string[] units = new String[3];
+for (int i = 0; i < units.Length; i++)
+{
+    Console.WriteLine("Please name your unit");
+    string userInput = Console.ReadLine();
+    units[i] = new string(userInput);
+}*/
 //Unit unit = new Unit("Skeleton", 100);
 //unit = new Unit("Zombie", 200);
 //unit = new Unit("Necromancer",300);
-Unit unit = new Unit("Leet", 1337);
-
-
-
-for(int i = 0; i < 3; i++)
-{
-    //new Unit("Leet",1337);
-    //Console.WriteLine("What do you want Leet's Health to be?");
-    //int newHealth = Convert.ToInt32(Console.ReadLine());
-    //unit.SetHealth(newHealth);
-}
-
 /*Testing Finalizer
 for(int i = 0; i < 2; i++)
 {
     unit = new Unit("LivingHand",400);
     GC.Collect();
 }*/
+for(int i = 0; i < 3; i++)
+{
+    new Unit("Leet",1337);
+    //Console.WriteLine("What do you want Leet's Health to be?");
+    //int newHealth = Convert.ToInt32(Console.ReadLine());
+    //unit.SetHealth(newHealth);
+}
 public class Unit
 {
     private string name;
@@ -28,12 +29,14 @@ public class Unit
     public static int nextId;
     private int maxHealth;
     private int health;
-
     public Unit(string name, int maxHealth)
     {
         this.name = name;
         this.maxHealth = maxHealth;
         SetHealth();
+        //Console.WriteLine("What do you want Leet's Health to be?");
+        //health = Convert.ToInt32(Console.ReadLine());
+        //health = Math.Clamp(health, 0, maxHealth);
         iD = nextId;
         nextId++;
         ReportStatus();// Make sure, that this is the last line of the constructor:
@@ -41,7 +44,8 @@ public class Unit
     public void SetHealth()
     {
         int newHealth;
-        Console.WriteLine($"What do you want {name}'s Health to be?");
+        Console.WriteLine("What do you want Leet's Health to be?");
+        //health = Convert.ToInt32(Console.ReadLine());
         newHealth = Convert.ToInt32(Console.ReadLine());
         health = newHealth;
         health = Math.Clamp(health, 0, maxHealth);

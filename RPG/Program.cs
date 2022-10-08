3a3258970e9b28cef69b19e109087ba34a206dd8
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-10-08 14:20:14 +0200

    back up rpg02 to rpg

--- RPG/Program.cs
+++ RPG/Program.cs
@@ -1,43 +1,47 @@
 ﻿// Lets try the RPG
 
-/*string[] units = new String[3];
-for (int i = 0; i < units.Length; i++)
-{
-    Console.WriteLine("Please name your unit");
-    string userInput = Console.ReadLine();
-    units[i] = new string(userInput);
-}*/
+Unit unit = new Unit("Skeleton", 100);
+unit = new Unit("Zombie", 200);
+unit = new Unit("Necromancer",300);
 
-Unit unit = new Unit("Skeleton");
-unit = new Unit("Zombie");
-unit = new Unit("Necromancer");
-
-//Testing Finalizer
+/*Testing Finalizer
 for(int i = 0; i < 2; i++)
 {
-    unit = new Unit("LivingHand");
+    unit = new Unit("LivingHand",400);
     GC.Collect();
-}
+}*/
 
+for(int i = 0; i < 3; i++)
+{
+    new Unit("Leet",1337);
+}
 public class Unit
 {
     private string name;
     public int iD;
     public static int nextId;
-    public Unit(string name)
+    private int maxHealth;
+    private int health;
+    public Unit(string name, int maxHealth)
     {
         this.name = name;
-        this.iD = nextId;
+        this.maxHealth = maxHealth;
+        SetHealth(health);
+        iD = nextId;
         nextId++;
         ReportStatus();// Make sure, that this is the last line of the constructor:
     }
-    public string GetName()
+    public int SetHealth(int newHealth)
     {
-        return name;
+        Console.WriteLine("What do you want Leet's Health to be?");
+        newHealth = Convert.ToInt32(Console.ReadLine());
+        health = newHealth;
+        health = Math.Clamp(health, 0, maxHealth);
+        return health;
     }
     public void ReportStatus()
     {
-        Console.WriteLine($"Unit #{iD}: {name}");
+        Console.WriteLine($"Unit #{iD}: {name} - {health}/{maxHealth} Health");
     }
     ~Unit()//Finalizer
     {

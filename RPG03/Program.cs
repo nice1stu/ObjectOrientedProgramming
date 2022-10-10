f49d2fac3dff8370f4f3bfd005dbeae67bad9e74
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-10-10 02:48:13 +0200

    copy working RPG02 to RPG03

--- RPG03/Program.cs
+++ RPG03/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Lets try the RPG
+
+Unit unit = new Unit("Skeleton", 100);
+unit = new Unit("Zombie", 200);
+unit = new Unit("Necromancer",300);
+
+do
+{
+    unit = new Unit("Leet",1337);
+    Console.WriteLine("What do you want Leet's Health to be?");
+    unit.Health = Convert.ToInt32(Console.ReadLine());
+}
+while (Unit.health > 0) ;
+
+public class Unit
+{
+    private string name;
+    public int iD;
+    public int nextId;
+    private int maxHealth;
+    public static int health;
+    public int Health
+    {
+        get 
+        {
+            return health;
+        }
+        set
+        {
+            health = Math.Clamp(value, 0, maxHealth);
+        }
+    }
+    public Unit(string name, int maxHealth)
+    {
+        this.name = name;
+        this.maxHealth = maxHealth;
+        iD = nextId;
+        nextId++;
+        ReportStatus();// Make sure, that this is the last line of the constructor:
+    }
+    public void ReportStatus()
+    {
+        Console.WriteLine($"Unit #{iD}: {name} - {health}/{maxHealth} Health");
+    }
+    ~Unit()//Finalizer
+    {
+        Console.WriteLine($"Unit #{iD}: {name} got finalized.");
+    }
+}

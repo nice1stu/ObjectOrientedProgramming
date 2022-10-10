41679f0aef56bef78d9db7238e0fe6cbc748505c
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-10-10 02:07:02 +0200

    Health Property working
    but report status in constructor is not

--- RPG02/Program.cs
+++ RPG02/Program.cs
@@ -20,7 +20,7 @@ for(int i = 0; i < 3; i++)
 {
     Unit unit = new Unit("Leet",1337);
     Console.WriteLine("What do you want Leet's Health to be?");
-    //unit.Health = Convert.ToInt32(Console.ReadLine());
+    unit.Health = Convert.ToInt32(Console.ReadLine());
     //unit.SetHealth(newHealth);
 }
 public class Unit
@@ -30,34 +30,34 @@ public class Unit
     public int nextId;
     private int maxHealth;
     private int health;
-    /*public int Health
+    public int Health
     {
         get 
         {
             return health;
         }
-        set 
+        set
         {
-            health = Math.Clamp(health, 0, maxHealth);
+            health = Math.Clamp(value, 0, maxHealth);
         }
-    }*/
+    }
     public Unit(string name, int maxHealth)
     {
         this.name = name;
         this.maxHealth = maxHealth;
-        SetHealth(health);
+        //SetHealth(health);
         iD = nextId;
         nextId++;
         ReportStatus();// Make sure, that this is the last line of the constructor:
     }
-    public int SetHealth(int newHealth)
+    /*public int SetHealth(int newHealth)
     {
         Console.WriteLine("What do you want Leet's Health to be?");
         newHealth = Convert.ToInt32(Console.ReadLine());
         health = newHealth;
         health = Math.Clamp(health, 0, maxHealth);
         return health;
-    }
+    }*/
     public void ReportStatus()
     {
         Console.WriteLine($"Unit #{iD}: {name} - {health}/{maxHealth} Health");

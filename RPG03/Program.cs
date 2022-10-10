777ca6db47f0b3fb0fb54828c2e8c1c0bea195dc
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-10-10 02:57:35 +0200

    get Name

--- RPG03/Program.cs
+++ RPG03/Program.cs
@@ -14,25 +14,20 @@ while (Unit.health > 0) ;
 
 public class Unit
 {
-    private string name;
     public int iD;
     public int nextId;
     private int maxHealth;
     public static int health;
+
+    public string Name { get; }
+
     public int Health
     {
-        get 
-        {
-            return health;
-        }
-        set
-        {
-            health = Math.Clamp(value, 0, maxHealth);
-        }
+        set => health = Math.Clamp(value, 0, maxHealth);
     }
-    public Unit(string name, int maxHealth)
+    public Unit(string Name, int maxHealth)
     {
-        this.name = name;
+        this.Name = Name;
         this.maxHealth = maxHealth;
         iD = nextId;
         nextId++;
@@ -40,10 +35,10 @@ public class Unit
     }
     public void ReportStatus()
     {
-        Console.WriteLine($"Unit #{iD}: {name} - {health}/{maxHealth} Health");
+        Console.WriteLine($"Unit #{iD}: {Name} - {health}/{maxHealth} Health");
     }
     ~Unit()//Finalizer
     {
-        Console.WriteLine($"Unit #{iD}: {name} got finalized.");
+        Console.WriteLine($"Unit #{iD}: {Name} got finalized.");
     }
 }

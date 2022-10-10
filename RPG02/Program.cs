0be6cc30bf91e438744d0cd1f8f9cc0fd774176a
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-10-10 02:36:16 +0200

    while loop works but no conditions set

--- RPG02/Program.cs
+++ RPG02/Program.cs
@@ -1,11 +1,5 @@
 ﻿// Lets try the RPG
-/*string[] units = new String[3];
-for (int i = 0; i < units.Length; i++)
-{
-    Console.WriteLine("Please name your unit");
-    string userInput = Console.ReadLine();
-    units[i] = new string(userInput);
-}*/
+
 //Unit unit = new Unit("Skeleton", 100);
 //unit = new Unit("Zombie", 200);
 //unit = new Unit("Necromancer",300);
@@ -13,21 +7,21 @@ for (int i = 0; i < units.Length; i++)
 Console.WriteLine("What do you want Leet's Health to be?");
 unit.Health = Convert.ToInt32(Console.ReadLine());
 unit.ReportStatus();*/
+
+while(true)
+{
+Unit unit = new Unit("Leet",1337);
+Console.WriteLine("What do you want Leet's Health to be?");
+unit.Health = Convert.ToInt32(Console.ReadLine());
+unit.ReportStatus();
+}
+
 /*Testing Finalizer
 for(int i = 0; i < 2; i++)
 {
     unit = new Unit("LivingHand",400);
     GC.Collect();
 }*/
-
-for(int i = 0; i < 3; i++)
-{
-Unit unit = new Unit("Leet",1337);
-Console.WriteLine("What do you want Leet's Health to be?");
-unit.Health = Convert.ToInt32(Console.ReadLine());
-unit.ReportStatus();
-    //unit.SetHealth(newHealth);
-}
 public class Unit
 {
     private string name;
@@ -50,19 +44,10 @@ public class Unit
     {
         this.name = name;
         this.maxHealth = maxHealth;
-        //SetHealth(health);
         iD = nextId;
         nextId++;
         //ReportStatus();// Make sure, that this is the last line of the constructor:
     }
-    /*public int SetHealth(int newHealth)
-    {
-        Console.WriteLine("What do you want Leet's Health to be?");
-        newHealth = Convert.ToInt32(Console.ReadLine());
-        health = newHealth;
-        health = Math.Clamp(health, 0, maxHealth);
-        return health;
-    }*/
     public void ReportStatus()
     {
         Console.WriteLine($"Unit #{iD}: {name} - {health}/{maxHealth} Health");

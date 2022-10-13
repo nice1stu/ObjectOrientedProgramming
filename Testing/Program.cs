b0827b4a8067ce975d43e35c39da8eac8f355f73
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-10-13 04:27:29 +0200

    Finlaisers

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -1,4 +1,26 @@
-﻿public class Animal
+﻿{
+    Main(new string[] { });
+}
+
+static void Main(string[] args)
 {
-    
+    for (var i = 0; i < 5; i++)
+    {
+        var animal = new Animal();
+        GC.Collect();
+        Thread.Sleep(1000);
+    }
+}
+
+public class Animal
+{
+    public Animal()
+    {
+        Console.WriteLine("animal is created");
+    }
+
+    ~Animal()
+    {
+        Console.WriteLine("animal is destroyed");
+    }
 }

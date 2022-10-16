9ebdc5307d1702a33f705a0f64f060f45da827d5
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-10-16 07:48:51 +0200

    add HasResurrected method

--- RPG04/Necromancer.cs
+++ RPG04/Necromancer.cs
@@ -6,21 +6,30 @@ override the Damage-Method in the Necromancer class
 Invoke the base-Method, to make sure, that the regular damage-dealing still works
 After that, check, whether the Necromancer's Health is zero or less and that he has NOT resurrected, yet
 If so: flag hasResurrected to be true, and set his Health to 50% of maxHealth again.
-Update the Game Loop, so a Necromancer is spawned instead of Leet*/
+Update the Game Loop, so a Necromancer is spawned instead of Leet
+
+Take the code that ressurects the Necromancer (set the health to 50% and change the flag)
+And move it to its own private Method named Resurrect
+Call this Method from within the Necromancer's Damage-Method instead of the two lines of code that currently do so*/
 
 public class Necromancer : Unit
 {
     bool hasResurrected = false;
-    
-    public Necromancer(string name, int maxHealth) : base(name, maxHealth){}
-    public override void Damage(int value)
+
+    private void HasResurrected()
     {
-        Health -= value;
         if (Health <= 0 && hasResurrected == false)
         {
             Console.WriteLine("I am born again!");
             Health = (int)(maxHealth * .5);
             hasResurrected = true;
         }
+    }
+    
+    public Necromancer(string name, int maxHealth) : base(name, maxHealth){}
+    public override void Damage(int value)
+    {
+        Health -= value;
+        HasResurrected();
     }
 }

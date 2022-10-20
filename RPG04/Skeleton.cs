0860e837221f166402863a72b51d355c58d488db
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-10-20 04:55:10 +0200

    change bonusDamage vs Skeleton to int field

--- RPG04/Skeleton.cs
+++ RPG04/Skeleton.cs
@@ -2,12 +2,13 @@
 
 public class Skeleton : Unit
 {
+    private int bonusDamage = 10;
     public Skeleton(string name, int maxHealth, Weapon weapon) : base(name, maxHealth, weapon) { }
     
     public override void TakeDamage(int value, Unit opponent)
     {
-        Health -= (value+10);
+        Health -= (value+bonusDamage);
         Console.WriteLine("The Hero deals 10 extra Damage against the Skeleton's weak Bones!");
-        Console.WriteLine($"The {this.Name} has taken {value+10} points of Damage from {opponent.Name}");
+        Console.WriteLine($"The {this.Name} has taken {value+bonusDamage} points of Damage from {opponent.Name}");
     }
 }

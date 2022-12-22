a6e8b1a1f2a0f2902e643f1c028d4f32d73378c2
nice1stu <112468923+nice1stu@users.noreply.github.com>
2022-12-22 16:20:48 +0100

    just testing stuff

--- RPG04/Weapon.cs
+++ RPG04/Weapon.cs
@@ -1,24 +1,20 @@
+public interface IWeapon
+{
+    int Power { get; }
+    string Name { get; }
+    IHand EquippedTo { get; }
+    void EquipTo(IHand hand);
+    void UnEquip();
+}
+
 public abstract class Weapon : IWeapon
 {
     public int Power { get; }
+    public abstract string Name { get; }
+    public IHand EquippedTo { get; private set; }
 
-    public IHand EquippedTo
-    {
-        get { return null; } //The public get Accessor exists, so other classes can also see, what Hand a Weapon is currently equipped to.
-        private set //The private set Accessor will be used to change the Hand that the Weapon is currently equipped to
-        {
-            //On EquippedTo-Property, assign null to the Weapon Property
-        }
-    }
-
-    public void EquipTo(IHand hand)
-    {
-
-    }
-    public void UnEquip()
-    {
-        EquippedTo = null;
-    }
+    public abstract void EquipTo(IHand hand);
+    public abstract void UnEquip();
 
     public Weapon(int power)
     {
@@ -28,30 +24,102 @@ public abstract class Weapon : IWeapon
 
 public class TrainingWeapon : Weapon
 {
-    public TrainingWeapon(int power) : base(power){}
+    public override string Name => "Training Weapon";
+
+    public override void EquipTo(IHand hand)
+    {
+        base.EquipTo(hand);
+    }
+
+    public override void UnEquip()
+    {
+        base.UnEquip();
+    }
+
+    public TrainingWeapon(int power) : base(power) { }
 }
 
 public class BoneSword : Weapon
 {
-    public BoneSword(int power) : base(power){}
+    public override string Name => "BoneSword";
+
+    public override void EquipTo(IHand hand)
+    {
+        base.EquipTo(hand);
+    }
+
+    public override void UnEquip()
+    {
+        base.UnEquip();
+    }
+
+    public BoneSword(int power) : base(power) { }
 }
 
 public class CursedStaff : Weapon
 {
-    public CursedStaff(int power) : base(power){}
+    public override string Name => "CursedStaff";
+
+    public override void EquipTo(IHand hand)
+    {
+        base.EquipTo(hand);
+    }
+
+    public override void UnEquip()
+    {
+        base.UnEquip();
+    }
+
+    public CursedStaff(int power) : base(power) { }
 }
 
 public class Unarmed : Weapon
 {
-    public Unarmed() : base(0){}
+    public override string Name => "Unarmed";
+
+    public override void EquipTo(IHand hand)
+    {
+        base.EquipTo(hand);
+    }
+
+    public override void UnEquip()
+    {
+        base.UnEquip();
+    }
+
+    public Unarmed(int power) : base(power) { }
 }
 
 public class Spike : Weapon
 {
-    public Spike(int power) : base(power){}
+    public override string Name => "Spike";
+
+    public override void EquipTo(IHand hand)
+    {
+        base.EquipTo(hand);
+    }
+
+    public override void UnEquip()
+    {
+        base.UnEquip();
+    }
+
+    public Spike(int power) : base(power) { }
 }
 
 public class Haunt : Weapon
 {
-    public Haunt(int power) : base(power){}
+    public override string Name => "Haunt";
+
+    public override void EquipTo(IHand hand)
+    {
+        base.EquipTo(hand);
+    }
+
+    public override void UnEquip()
+    {
+        base.UnEquip();
+    }
+
+    public Haunt(int power) : base(power) { }
 }

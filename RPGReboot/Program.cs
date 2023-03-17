a02da0cd8edead2375247d9497927a3a403f7cf9
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-03-17 01:23:32 +0100

    Update Program.cs
    Completed  2 Encapsulation

--- RPGReboot/Program.cs
+++ RPGReboot/Program.cs
@@ -8,65 +8,64 @@ for (int i = 0; i < 4; i++)
 unit = new Unit("Leet", Unit.NextId, 1337);
 unit.ReportStatus();
 
-while (unit.SetHealth > 0)
+while (unit.Health > 0)
 {
-    Console.WriteLine("What do you want Leet's Health to be?");
+    Console.WriteLine("How much damage do you want to deal to Leet?");
     string? userInput = Console.ReadLine();
     if (userInput != null)
     {
-        int leetHealth = int.Parse(userInput);
-        unit.SetHealth = leetHealth;
+        int damage = int.Parse(userInput);
+        unit.Damage(damage);
     }
     unit.ReportStatus();
 }
 
-/*for(int i = 0; i < 2; i++)
-{
-    Unit unit = new Unit("LivingHand", i);
-    GC.Collect();
-}*/
-
 class Unit
 {
-    private string _name;
-    private int _id;
-    private int _maxHealth;
+    private readonly int _id;
+    private readonly int _maxHealth;
     private int _health;
-    public static int NextId { get; set; }
+    public static int NextId { get; private set; }
 
-    public Unit(string name, int id, int _maxHealth)
+    public Unit(string name, int id, int maxHealth)
     {
-        this._name = name;
-        this._maxHealth = _maxHealth;
-        _health = _maxHealth;
+        this.Name = name;
+        this._maxHealth = maxHealth;
+        _health = maxHealth;
         _id = id;
         NextId++;
     }
 
-    public int SetHealth
+    private string Name { get; }
+
+    protected internal int Health
+    {
+        get => _health;
+        set => _health = Math.Min(Math.Max(0, value), _maxHealth);
+    }
+
+    public void Damage(int value)
+    {
+        Health = Health - value;
+    }
+
+    bool IsAlive
     {
         get
         {
-            return _health;
-        }
-        set
-        {
-            _health = value;
-            if (_health <= 0 || _health > _maxHealth)
-            {
-                _health = Math.Min(Math.Max(0, value), _maxHealth);
-            }
+            if(_health > 0) return true;
+            return false;
         }
     }
-    
+
     public void ReportStatus()
     {
-        Console.WriteLine($"Unit #{_id}: {_name} - {_health}/{_maxHealth} Health" );
+        Console.WriteLine($"Unit #{_id}: {Name} - {_health}/{_maxHealth} Health" );
     }
     
     ~Unit()
     {
-        Console.WriteLine($"Unit #{_id}: {_name} got finalized");
+        Console.WriteLine($"Unit #{_id}: {Name} got finalized");
     }
     // Make sure, that this is the last line of the constructor:
 }
